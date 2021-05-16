22971c66c21d18838aab988626c8bf6e0c7e3c1f
Joma Simeon <simjoms@gmail.com>
2021-05-16 11:51:07 +0300

    dynamic loading editor netxjs

--- src/pages/post-job.js
+++ src/pages/post-job.js
@@ -1,30 +1,19 @@
-import React from 'react'
-import { CKEditor } from '@ckeditor/ckeditor5-react'
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
-
+import React, { useRef, useState, useEffect } from 'react'
 import PageWrapper from '../components/PageWrapper'
 
-const defaultTypes = [
-  { value: 'b2b', label: 'B2B' },
-  { value: 'saas', label: 'SAAS' },
-  { value: 'b2b', label: 'b2b' }
-]
-
-const defaultEmployees = [
-  { value: '10-50', label: '10-50' },
-  { value: '50-100', label: '50-100' },
-  { value: '100-500', label: '100-500' },
-  { value: '500-2000', label: '500-2000' }
-]
-
-const defaultLocations = [
-  { value: 'bd', label: 'Bangladesh' },
-  { value: 'sp', label: 'Singapore' },
-  { value: 'tl', label: 'Thailand' },
-  { value: 'de', label: 'Germany' }
-]
-
 const DashboardSettings = () => {
+  const editorRef = useRef()
+  const [editorLoaded, setEditorLoaded] = useState(false)
+  const { CKEditor, ClassicEditor } = editorRef.current || {}
+
+  useEffect(() => {
+    editorRef.current = {
+      CKEditor: require('@ckeditor/ckeditor5-react').CKEditor,
+      ClassicEditor: require('@ckeditor/ckeditor5-build-classic')
+    }
+    setEditorLoaded(true)
+  }, [])
+
   return (
     <>
       <PageWrapper
@@ -47,24 +36,28 @@ const DashboardSettings = () => {
                     New job
                   </h5>
                   <div className='contact-form bg-white shadow-8 rounded-4 pl-sm-10 pl-4 pr-sm-11 pr-4 pt-15 pb-13'>
-                    <CKEditor
-                      editor={ClassicEditor}
-                      data='<p>Hello welcome to study counter. Start typing here ....</p>'
-                      onReady={editor => {
-                        // You can store the "editor" and use when it is needed.
-                        console.log('Editor is ready to use!', editor)
-                      }}
-                      onChange={(event, editor) => {
-                        const data = editor.getData()
-                        console.log({ event, editor, data })
-                      }}
-                      onBlur={(event, editor) => {
-                        console.log('Blur.', editor)
-                      }}
-                      onFocus={(event, editor) => {
-                        console.log('Focus.', editor)
-                      }}
-                    />
+                    {editorLoaded ? (
+                      <CKEditor
+                        editor={ClassicEditor}
+                        data='<p>Hello welcome to study counter. Start typing here ....</p>'
+                        onReady={editor => {
+                          // You can store the "editor" and use when it is needed.
+                          console.log('Editor is ready to use!', editor)
+                        }}
+                        onChange={(event, editor) => {
+                          const data = editor.getData()
+                          console.log({ event, editor, data })
+                        }}
+                        onBlur={(event, editor) => {
+                          console.log('Blur.', editor)
+                        }}
+                        onFocus={(event, editor) => {
+                          console.log('Focus.', editor)
+                        }}
+                      />
+                    ) : (
+                      <div>Editor loading</div>
+                    )}
                   </div>
                 </div>
               </div>
