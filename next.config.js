d380330b4529721b9e50b7ffed050da6645ae92b
Joma Simeon <simjoms@gmail.com>
2021-09-19 13:10:25 +0300

    update next configs

--- next.config.js
+++ next.config.js
@@ -1,9 +1,31 @@
-const withOptimizedImages = require("next-optimized-images");
-const withFonts = require("next-fonts");
+const withOptimizedImages = require('next-optimized-images')
+const withFonts = require('next-fonts')
+require('dotenv').config()
+const Dotenv = require('dotenv-webpack')
+
+const path = require('path')
 
 module.exports = withFonts(
   withOptimizedImages({
     /* config for next-optimized-images */
     // your config for other plugins or the general next.js here...
-  })
-);
+  }),
+  [
+    new Dotenv({
+      path: path.join(__dirname, '.env'),
+      systemvars: true
+    })
+  ],
+  {
+    env: {
+      NEXT_API_URL: process.env.NEXT_API_URL,
+      NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME:
+        process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+      NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET:
+        process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET,
+      NEXT_PUBLIC_CLOUDINARY_API_KEY:
+        process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
+      CLOUDINARY_SECRET_KEY: process.env.CLOUDINARY_SECRET_KEY
+    }
+  }
+)
